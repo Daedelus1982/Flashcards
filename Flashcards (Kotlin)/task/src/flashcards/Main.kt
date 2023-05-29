add015f230aac0a972549847ca8f4a1ba5d52c89
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-05-29 15:08:02 +0100

    stage 2 - check answer

--- Flashcards (Kotlin)/task/src/flashcards/Main.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Main.kt
@@ -1,8 +1,15 @@
 package flashcards
 
 fun main() {
-    println("Card:")
-    println("cos'(x)")
-    println("Definition:")
-    println("-sin(x)")
+    val definition = createCard(readln(), readln())
+
+    val answer = readln()
+
+    val rightOrWrong = checkAnswer(definition, answer)
+
+    println("Your answer is $rightOrWrong")
 }
+
+fun createCard(question: String, definition: String): String = definition
+
+fun checkAnswer(definition: String, answer: String) = if (definition == answer) "right" else "wrong"
