ffc5dc8fac32920725efd7654740eaffe8b245b6
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-06-07 19:06:05 +0100

    stage 6 done

--- Flashcards (Kotlin)/task/src/flashcards/Main.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Main.kt
@@ -6,96 +6,133 @@ import kotlin.random.Random
 
 fun main() {
     val flashCards = mutableMapOf<String, Pair<String, Int>>()
+    val logList = mutableListOf<String>()
+
+    val myPrintln : (String) -> Unit = {
+        logList.add(it)
+        println(it)
+    }
+
+    val myRead : () -> String = {
+        val input = readln()
+        logList.add("> $input")
+        input
+    }
+
     do {
-        println("Input the action (add, remove, import, export, ask, exit):")
-        val response = readln()
+        myPrintln("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
+        val response = myRead()
 
         when(response) {
-            "add" -> add(flashCards)
-            "remove" -> remove(flashCards)
-            "import" -> import(flashCards)
-            "export" -> export(flashCards)
-            "ask" -> ask(flashCards)
+            "add" -> add(flashCards, myPrintln, myRead)
+            "remove" -> remove(flashCards, myPrintln, myRead)
+            "import" -> import(flashCards, myPrintln, myRead)
+            "export" -> export(flashCards, myPrintln, myRead)
+            "ask" -> ask(flashCards, myPrintln, myRead)
+            "log" -> log(logList, myPrintln, myRead)
+            "hardest card" -> hardest(flashCards,myPrintln)
+            "reset stats" -> resetStats(flashCards, myPrintln)
         }
-        println() // add a blank line after each task
+        myPrintln("") // // add a blank line after each task
     } while (response != "exit")
 
-    println("Bye bye!")
+    myPrintln("Bye bye!")
 }
 
-fun add(flashCards: MutableMap<String, Pair<String, Int>>) {
-    println("The card:")
-    val term = readln()
-    if (flashCards.contains(term)) println("The card \"$term\" already exists.")
+fun log(logList: MutableList<String>, myPrintln: (String) -> Unit, myRead: () -> String) {
+    myPrintln("File name:")
+    val filename = myRead()
+    val file = File(filename)
+    myPrintln("The log has been saved.")
+    file.writeText(logList.joinToString("\n") { it })
+}
+
+
+fun add(flashCards: MutableMap<String, Pair<String, Int>>,
+        myPrintln: (String) -> Unit,
+        myRead: () -> String) {
+    myPrintln("The card:")
+    val term = myRead()
+    if (flashCards.contains(term)) myPrintln("The card \"$term\" already exists.")
     else {
-        println("The definition of the card:")
-        val definition = readln()
+        myPrintln("The definition of the card:")
+        val definition = myRead()
+
         if (flashCards
             .values
             .map { it.first }
-            .contains(definition)) print("The definition \"$definition\" already exists.")
+            .contains(definition)) myPrintln("The definition \"$definition\" already exists.")
         else {
             flashCards[term] = Pair(definition, 0)
-            println("The pair (\"$term\":\"$definition\") has been added.")
+            myPrintln("The pair (\"$term\":\"$definition\") has been added.")
         }
     }
 }
 
-fun remove(flashCards: MutableMap<String, Pair<String, Int>>) {
-    println("Which card?")
-    val term = readln()
-    if (!flashCards.contains(term)) println("can't remove \"$term\": there is no such card.")
+fun remove(flashCards: MutableMap<String, Pair<String, Int>>,
+           myPrintln: (String) -> Unit, myRead: () -> String) {
+    myPrintln("Which card?")
+    val term = myRead()
+    if (!flashCards.contains(term)) myPrintln("can't remove \"$term\": there is no such card.")
     else {
         flashCards.remove(term)
-        println("The card has been removed.")
+        myPrintln("The card has been removed.")
     }
 }
 
-fun import(flashCards: MutableMap<String, Pair<String, Int>>) {
-    println("File name:")
-    val filename = readln()
+fun import(flashCards: MutableMap<String, Pair<String, Int>>,
+           myPrintln: (String) -> Unit, myRead: () -> String) {
+    myPrintln("File name:")
+    val filename = myRead()
     val file = File(filename)
-    if (!file.exists()) println("File not found.")
+    if (!file.exists()) myPrintln("File not found.")
     else {
-        val fileLines = file.readLines().filter { it.isNotEmpty() }
+        val fileLines = file
+            .readLines()
+            .filter { it.isNotEmpty() }
         for (fileLine in fileLines) {
             val (k, v) = fileLine.split("=")
-            val (definition, mistakes) = v.removePrefix("(").removeSuffix(")").split(", ")
+            val (definition, mistakes) = v
+                .removePrefix("(")
+                .removeSuffix(")")
+                .split(", ")
             flashCards[k] = definition to mistakes.toInt()
         }
-        println("${fileLines.size} cards have been loaded.")
+        myPrintln("${fileLines.size} cards have been loaded.")
     }
 }
 
-fun export(flashCards: MutableMap<String, Pair<String, Int>>) {
-    println("File name:")
-    val filename = readln()
+fun export(flashCards: MutableMap<String, Pair<String, Int>>,
+           myPrintln: (String) -> Unit, myRead: () -> String) {
+    myPrintln("File name:")
+    val filename = myRead()
     val file = File(filename)
     if (flashCards.isNotEmpty()) {
         file.delete()
         file.createNewFile()
     }
     for (card in flashCards) file.appendText("$card\n")
-    println("${flashCards.size} cards have been saved.")
+    myPrintln("${flashCards.size} cards have been saved.")
 }
 
-fun ask(flashCards: MutableMap<String, Pair<String, Int>>) {
-    println("How many times to ask?")
-    val questionCount = readln().toInt()
+fun ask(flashCards: MutableMap<String, Pair<String, Int>>,
+        myPrintln: (String) -> Unit, myRead: () -> String) {
+    myPrintln("How many times to ask?")
+    val questionCount = myRead().toInt()
     repeat(questionCount) {
         val term = flashCards.keys.toList()[Random.nextInt(0, flashCards.size)]
         val definition = flashCards[term]!!.first
-        println("Print the definition of \"$term\":")
-        val answer = readln()
+        myPrintln("Print the definition of \"$term\":")
+        val answer = myRead()
         val otherTerm = getTermForDefinition(answer, flashCards)
         if (answer == definition) {
-            println("Correct!")
+            myPrintln("Correct!")
         } else if (otherTerm.isNotEmpty()) {
             addMistake(term, flashCards)
-            println("Wrong. The right answer is \"$definition\", but your definition is correct for \"$otherTerm\"")
+            myPrintln("Wrong. The right answer is \"$definition\", but your definition is correct for \"$otherTerm\"")
         } else {
             addMistake(term, flashCards)
-            println("Wrong. The right answer is \"$definition\".")
+            myPrintln("Wrong. The right answer is \"$definition\".")
         }
     }
 }
@@ -113,4 +150,33 @@ fun getTermForDefinition(definition: String, cards: MutableMap<String, Pair<Stri
 fun addMistake(term: String, flashCards: MutableMap<String, Pair<String, Int>>) {
     val (definition, mistakes) = flashCards[term]!!
     flashCards[term] = definition to mistakes + 1
+}
+
+fun resetStats(flashCards: MutableMap<String, Pair<String, Int>>,
+               myPrintln: (String) -> Unit) {
+    for (card in flashCards) flashCards[card.key] = card.value.first to 0
+    myPrintln("Card statistics have been reset.")
+}
+
+fun hardest(flashCards: MutableMap<String, Pair<String, Int>>, myPrintln: (String) -> Unit) {
+    if (flashCards.isEmpty()) myPrintln("There are no cards with errors.")
+    else {
+        val mostMistakes = flashCards
+            .maxOf { it.value.second }
+        val cardsWithMostMistakes = if (mostMistakes == 0) emptySet<String>()
+        else flashCards
+            .filter { it.value.second == mostMistakes }
+            .keys
+            .map { "\"$it\"" }
+        if (cardsWithMostMistakes.isEmpty()) myPrintln("There are no cards with errors.")
+        else if (cardsWithMostMistakes.size > 1) myPrintln(
+            "The hardest cards are \"${cardsWithMostMistakes.joinToString(", ")} " +
+                    "You have $mostMistakes errors answering them."
+        )
+        else // must be one card with the highest mistakes.
+            myPrintln(
+                "The hardest card is ${cardsWithMostMistakes.first()}. " +
+                        "You have $mostMistakes errors answering it."
+            )
+    }
 }
